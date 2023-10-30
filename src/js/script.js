ed01ea9c92f99c061b858f0244ee2e684de7a5a5
Onvix24 <maksdark2004@gmail.com>
2023-10-30 12:21:49 +0200

    added a modal window and a description on the card when hovering over the card in the portfolio

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,39 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+        contactLink: document.getElementById('contact')
+    };
 
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+
+        const isModalOpen = !refs.modal.classList.contains("is-hidden");
+        document.body.classList.toggle("no-scroll", isModalOpen);
+
+        if (isModalOpen) {
+            homeLink.classList.remove("nav__link_active");
+            portfolioLink.classList.remove("nav__link_active");
+        }
+
+        refs.contactLink.classList.toggle("nav__link_active", isModalOpen)
+    }
+})();
+
+//=================================
+
+// Отримати поточну URL сторінки
+const currentPageUrl = window.location.href;
+// Отримати посилання
+const homeLink = document.getElementById("studio");
+const portfolioLink = document.getElementById("portfolio");
+
+if (currentPageUrl.includes("index.html")) {
+    homeLink.classList.add("nav__link_active");
+} else if (currentPageUrl.includes("portfolio.html")) {
+    portfolioLink.classList.add("nav__link_active");
+}
