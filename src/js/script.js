bacdeb3f1775da9d94b02e5d22461117b1f53d91
Onvix24 <maksdark2004@gmail.com>
2023-11-24 13:25:25 +0200

    fix problem

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,55 @@
 (() => {
     const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-        contactLink: document.getElementById('contact')
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
     };
-
+  
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  
     function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-
-        const isModalOpen = !refs.modal.classList.contains("is-hidden");
-        document.body.classList.toggle("no-scroll", isModalOpen);
-
-        if (isModalOpen) {
-            homeLink.classList.remove("nav__link_active");
-            portfolioLink.classList.remove("nav__link_active");
-        }
-
-        refs.contactLink.classList.toggle("nav__link_active", isModalOpen)
+      refs.modal.classList.toggle("is-hidden");
     }
-})();
+  })();
 
-//=================================
+// (() => {
+//     const refs = {
+//         openModalBtn: document.querySelector("[data-modal-open]"),
+//         closeModalBtn: document.querySelector("[data-modal-close]"),
+//         modal: document.querySelector("[data-modal]"),
+//         contactLink: document.getElementById('contact')
+//     };
 
-// Отримати поточну URL сторінки
-const currentPageUrl = window.location.href;
-// Отримати посилання
-const homeLink = document.getElementById("studio");
-const portfolioLink = document.getElementById("portfolio");
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
 
-if (currentPageUrl.includes("index.html")) {
-    homeLink.classList.add("nav__link_active");
-} else if (currentPageUrl.includes("portfolio.html")) {
-    portfolioLink.classList.add("nav__link_active");
-}
+//     function toggleModal() {
+//         refs.modal.classList.toggle("is-hidden");
+
+//         const isModalOpen = !refs.modal.classList.contains("is-hidden");
+//         document.body.classList.toggle("no-scroll", isModalOpen);
+
+//         if (isModalOpen) {
+//             homeLink.classList.remove("nav__link_active");
+//             portfolioLink.classList.remove("nav__link_active");
+//         }
+
+//         refs.contactLink.classList.toggle("nav__link_active", isModalOpen)
+//     }
+// })();
+
+// //=================================
+
+// // Отримати поточну URL сторінки
+// const currentPageUrl = window.location.href;
+// // Отримати посилання
+// const homeLink = document.getElementById("studio");
+// const portfolioLink = document.getElementById("portfolio");
+
+// if (currentPageUrl.includes("index.html")) {
+//     homeLink.classList.add("nav__link_active");
+// } else if (currentPageUrl.includes("portfolio.html")) {
+//     portfolioLink.classList.add("nav__link_active");
+// }
+
